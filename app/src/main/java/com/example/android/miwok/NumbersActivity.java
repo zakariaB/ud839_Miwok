98cf05acf20d3838f19416d144224b196d2f5a6a
zakariaB <zakaria.bouhoun@gmail.com>
2017-04-28 12:09:39 +0100

    add list of numbers

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,16 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +34,18 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        List<String> words = new ArrayList<>(Arrays.
+                asList("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten")
+        );
+
+        LinearLayout viewById = (LinearLayout) findViewById(R.id.rootView);
+
+        for (String w : words){
+            TextView textView = new TextView(this);
+            textView.setText(w);
+            viewById.addView(textView);
+        }
+
     }
 }
